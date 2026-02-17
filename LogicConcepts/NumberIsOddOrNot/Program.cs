3d8a2536ce3dd04662a336b1ba21acb6fc25f642
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-02-17 16:16:15 -0500

    Create blank solution

---
+++ LogicConcepts/NumberIsOddOrNot/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Timers;
+
+Console.Write("Igrese número!: ");
+var numberString = Console.ReadLine();
+var numberInt = int.Parse(numberString);
+if (numberInt % 2 == 0)
+{
+    Console.WriteLine($"El número: {numberInt} es par");
+}
+else
+{
+    Console.WriteLine($"El número: {numberInt} es impar");
+}
+
